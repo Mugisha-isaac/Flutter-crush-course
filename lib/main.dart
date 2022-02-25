21a452e31a5523ecdf4efcfbee5331e565733123
Mugisha-isaac <mugishaisaac2001@gmail>
2022-02-25 09:45:13 +0200

    styling buttons

--- lib/main.dart
+++ lib/main.dart
@@ -16,27 +16,48 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                "what do you want. a pencil with 2 erasers..?",
-                style: TextStyle(fontSize: 27.5, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+              Container(
+                margin: EdgeInsets.fromLTRB(15, 5, 15, 45),
+                child: Text(
+                  "what do you want. a pencil with 2 erasers..?",
+                  style: TextStyle(fontSize: 27.5, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
               ),
-              Text(
-                "Pointless",
-                style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.normal),
-                textAlign: TextAlign.center,
+              Container(
+                margin: EdgeInsets.all(25),
+                child: Text(
+                  "Pointless",
+                  style:
+                      TextStyle(fontSize: 22.5, fontWeight: FontWeight.normal),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  FloatingActionButton(
-                    onPressed: null,
-                    child: Icon(Icons.arrow_left_rounded),
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: FloatingActionButton(
+                      onPressed: null,
+                      child: Icon(
+                        Icons.arrow_left_rounded,
+                        size: 60,
+                      ),
+                      backgroundColor: Colors.orange,
+                    ),
                   ),
-                  FloatingActionButton(
-                    onPressed: null,
-                    child: Icon(Icons.arrow_right_rounded),
-                  )
+                  Container(
+                    margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                    child: FloatingActionButton(
+                      onPressed: null,
+                      child: Icon(
+                        Icons.arrow_right_rounded,
+                        size: 60,
+                      ),
+                      backgroundColor: Colors.orange,
+                    ),
+                  ),
                 ],
               ),
             ],
