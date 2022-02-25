c020e80ef460d2337cee25da57b28e5c7c123491
Mugisha-isaac <mugishaisaac2001@gmail>
2022-02-25 09:19:38 +0200

    styling the ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,8 +10,39 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("my app"),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "what do you want. a pencil with 2 erasers..?",
+                style: TextStyle(fontSize: 27.5, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Pointless",
+                style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.normal),
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  FloatingActionButton(
+                    onPressed: null,
+                    child: Icon(Icons.arrow_left_rounded),
+                  ),
+                  FloatingActionButton(
+                    onPressed: null,
+                    child: Icon(Icons.arrow_right_rounded),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
