fa02b6a2662260331292002755ec6b23d3114cec
Mugisha-isaac <mugishaisaac2001@gmail>
2022-02-25 08:36:31 +0200

    building the ui

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text("my app"),
+    );
+  }
+}
